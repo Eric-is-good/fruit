6986efb05fa12ce8f5accabb7ade880200d00ab8
Eric <eric123456123456@163.com>
2022-01-24 14:41:43 +0800

    第一阶段
    已完成功能：
    mysql读取与改动
    网页分页

---
+++ src/main/java/fruit/servlets/IndexServlet.java
@@ -0,0 +1,52 @@
+package fruit.servlets;
+
+import fruit.dao.FruitDAO;
+import fruit.dao.impl.FruitDAOImpl;
+import fruit.pojo.Fruit;
+import myssm.myspringmvc.ViewBaseServlet;
+import myssm.util.StringUtil;
+import org.junit.Test;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+//Servlet从3.0版本开始支持注解方式的注册
+@WebServlet("/index")
+public class IndexServlet extends ViewBaseServlet {
+    @Override
+    public void doGet(HttpServletRequest request , HttpServletResponse response)throws IOException, ServletException {
+        Integer pageNo = 1 ;
+        String pageNoStr = request.getParameter("pageNo");
+        if(StringUtil.isNotEmpty(pageNoStr)){
+            pageNo = Integer.parseInt(pageNoStr);
+        }
+
+        HttpSession session = request.getSession() ;
+        session.setAttribute("pageNo",pageNo);
+
+        FruitDAO fruitDAO = new FruitDAOImpl();
+        List<Fruit> fruitList = fruitDAO.getFruitList(pageNo);
+
+        session.setAttribute("fruitList",fruitList);
+
+        //总记录条数
+        int fruitCount = fruitDAO.getFruitCount();
+        //总页数
+        int pageCount = (fruitCount+5-1)/5 ;
+
+        session.setAttribute("pageCount",pageCount);
+
+        //此处的视图名称是 index
+        //那么thymeleaf会将这个 逻辑视图名称 对应到 物理视图 名称上去
+        //逻辑视图名称 ：   index
+        //物理视图名称 ：   view-prefix + 逻辑视图名称 + view-suffix
+        //所以真实的视图名称是：      /       index       .html
+        super.processTemplate("index",request,response);
+    }
+}
