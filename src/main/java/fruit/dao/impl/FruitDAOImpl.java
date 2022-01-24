f7e0b79141d54865839e0810e4f8bbc0a526bf0c
Eric <eric123456123456@163.com>
2022-01-24 14:41:43 +0800

    第一阶段
    已完成功能：
    mysql读取与改动
    网页分页

---
+++ src/main/java/fruit/dao/impl/FruitDAOImpl.java
@@ -0,0 +1,41 @@
+package fruit.dao.impl;
+
+import fruit.dao.FruitDAO;
+import fruit.pojo.Fruit;
+import myssm.basedao.BaseDAO;
+
+import java.util.List;
+
+public class FruitDAOImpl extends BaseDAO<Fruit> implements FruitDAO {
+    @Override
+    public List<Fruit> getFruitList(Integer pageNo) {
+        return super.executeQuery("select * from fruit_data limit ? , 5" , (pageNo-1)*5);
+    }
+
+    @Override
+    public Fruit getFruitByFid(Integer fid) {
+        return super.load("select * from fruit_data where fid = ? " , fid);
+    }
+
+    @Override
+    public void updateFruit(Fruit fruit) {
+        String sql = "update fruit_data set fname = ? , price = ? , fcount = ? , remark = ? where fid = ? " ;
+        super.executeUpdate(sql,fruit.getFname(),fruit.getPrice(),fruit.getFcount(),fruit.getRemark(),fruit.getFid());
+    }
+
+    @Override
+    public void delFruit(Integer fid) {
+        super.executeUpdate("delete from fruit_data where fid = ? " , fid) ;
+    }
+
+    @Override
+    public void addFruit(Fruit fruit) {
+        String sql = "insert into fruit_data values(0,?,?,?,?)";
+        super.executeUpdate(sql,fruit.getFname(),fruit.getPrice(),fruit.getFcount(),fruit.getRemark());
+    }
+
+    @Override
+    public int getFruitCount() {
+        return ((Long)super.executeComplexQuery("select count(*) from fruit_data")[0]).intValue();
+    }
+}
---
+++ src/main/java/fruit/servlets/EditServlet.java
@@ -0,0 +1,30 @@
+package fruit.servlets;
+
+import fruit.dao.FruitDAO;
+import fruit.dao.impl.FruitDAOImpl;
+import fruit.pojo.Fruit;
+import myssm.myspringmvc.ViewBaseServlet;
+import myssm.util.StringUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/edit.do")
+public class EditServlet extends ViewBaseServlet {
+
+    private FruitDAO fruitDAO = new FruitDAOImpl();
+
+    @Override
+    public void doGet(HttpServletRequest request , HttpServletResponse response)throws IOException, ServletException {
+        String fidStr = request.getParameter("fid");
+        if(StringUtil.isNotEmpty(fidStr)){
+            int fid = Integer.parseInt(fidStr);
+            Fruit fruit = fruitDAO.getFruitByFid(fid);
+            request.setAttribute("fruit",fruit);
+            super.processTemplate("edit",request,response);
+        }
+    }
+}
