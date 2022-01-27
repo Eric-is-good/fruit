1f38128deadd4d2ca2cd4d14d9fa42dc1db6d0b6
Eric <eric123456123456@163.com>
2022-01-27 21:49:17 +0800

    将 重定向 和 FruitServlet的输入解析 集合在了中央控制器里面
    编译使用了 -parameters，获得实际形参名

--- src/main/java/fruit/controllers/FruitController.java
+++ src/main/java/fruit/controllers/FruitController.java
@@ -15,111 +15,54 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
-public class FruitController extends ViewBaseServlet {
 
-    //之前FruitServlet是一个Sevlet组件，那么其中的init方法一定会被调用
-    //之前的init方法内部会出现一句话：super.init();
-
-    private ServletContext servletContext ;
-
-    public void setServletContext(ServletContext servletContext) throws ServletException {
-        this.servletContext = servletContext;
-        super.init(servletContext);
-    }
-
+public class FruitController {
     private FruitDAO fruitDAO = new FruitDAOImpl();
 
-    private void update(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //1.设置编码
-        request.setCharacterEncoding("utf-8");
-
-        //2.获取参数
-        String fidStr = request.getParameter("fid");
-        Integer fid = Integer.parseInt(fidStr);
-        String fname = request.getParameter("fname");
-        String priceStr = request.getParameter("price");
-        int price = Integer.parseInt(priceStr);
-        String fcountStr = request.getParameter("fcount");
-        Integer fcount = Integer.parseInt(fcountStr);
-        String remark = request.getParameter("remark");
-
+    private String update(Integer fid , String fname , Integer price , Integer fcount , String remark ){
         //3.执行更新
         fruitDAO.updateFruit(new Fruit(fid,fname, price ,fcount ,remark ));
-
         //4.资源跳转
-        //super.processTemplate("index",request,response);
-        //request.getRequestDispatcher("index.html").forward(request,response);
-        //此处需要重定向，目的是重新给IndexServlet发请求，重新获取furitList，然后覆盖到session中，这样index.html页面上显示的session中的数据才是最新的
-        response.sendRedirect("fruit.do");
+        return "redirect:fruit.do";
     }
 
-    private void edit(HttpServletRequest request , HttpServletResponse response)throws IOException, ServletException {
-        String fidStr = request.getParameter("fid");
-        if(StringUtil.isNotEmpty(fidStr)){
-            int fid = Integer.parseInt(fidStr);
+    private String edit(Integer fid , HttpServletRequest request){
+        if(fid!=null){
             Fruit fruit = fruitDAO.getFruitByFid(fid);
             request.setAttribute("fruit",fruit);
-            super.processTemplate("edit",request,response);
+            //super.processTemplate("edit",request,response);
+            return "edit";
         }
+        return "error" ;
     }
 
-    private void del(HttpServletRequest request , HttpServletResponse response)throws IOException, ServletException {
-        String fidStr = request.getParameter("fid");
-        if(StringUtil.isNotEmpty(fidStr)){
-            int fid = Integer.parseInt(fidStr);
+    private String del(Integer fid  ){
+        if(fid!=null){
             fruitDAO.delFruit(fid);
-
-            //super.processTemplate("index",request,response);
-            response.sendRedirect("fruit.do");
+            return "redirect:fruit.do";
         }
+        return "error";
     }
 
-    private void add(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
-
-        String fname = request.getParameter("fname");
-        Integer price = Integer.parseInt(request.getParameter("price")) ;
-        Integer fcount = Integer.parseInt(request.getParameter("fcount"));
-        String remark = request.getParameter("remark");
-
+    private String add(String fname , Integer price , Integer fcount , String remark ) {
         Fruit fruit = new Fruit(0,fname , price , fcount , remark ) ;
-
         fruitDAO.addFruit(fruit);
-
-        response.sendRedirect("fruit.do");
-
+        return "redirect:fruit.do";
     }
 
-    private void index(HttpServletRequest request , HttpServletResponse response)throws IOException, ServletException {
+    private String index(String oper , String keyword , Integer pageNo , HttpServletRequest request ) {
         HttpSession session = request.getSession() ;
 
-        // 设置当前页，默认值1
-        Integer pageNo = 1 ;
-
-        String oper = request.getParameter("oper");
-
-        //如果oper!=null 说明 通过表单的查询按钮点击过来的
-        //如果oper是空的，说明 不是通过表单的查询按钮点击过来的
-        String keyword = null ;
+        if(pageNo==null){
+            pageNo = 1;
+        }
         if(StringUtil.isNotEmpty(oper) && "search".equals(oper)){
-            //说明是点击表单查询发送过来的请求
-            //此时，pageNo应该还原为1 ， keyword应该从请求参数中获取
             pageNo = 1 ;
-            keyword = request.getParameter("keyword");
-            //如果keyword为null，需要设置为空字符串""，否则查询时会拼接成 %null% , 我们期望的是 %%
             if(StringUtil.isEmpty(keyword)){
                 keyword = "" ;
             }
-            //将keyword保存（覆盖）到session中
             session.setAttribute("keyword",keyword);
         }else{
-            //说明此处不是点击表单查询发送过来的请求（比如点击下面的上一页下一页或者直接在地址栏输入网址）
-            //此时keyword应该从session作用域获取
-            String pageNoStr = request.getParameter("pageNo");
-            if(StringUtil.isNotEmpty(pageNoStr)){
-                pageNo = Integer.parseInt(pageNoStr);   //如果从请求中读取到pageNo，则类型转换。否则，pageNo默认就是1
-            }
-            //如果不是点击的查询按钮，那么查询是基于session中保存的现有keyword进行查询
             Object keywordObj = session.getAttribute("keyword");
             if(keywordObj!=null){
                 keyword = (String)keywordObj ;
@@ -139,22 +82,8 @@ public class FruitController extends ViewBaseServlet {
         int fruitCount = fruitDAO.getFruitCount(keyword);
         //总页数
         int pageCount = (fruitCount+5-1)/5 ;
-        /*
-        总记录条数       总页数
-        1               1
-        5               1
-        6               2
-        10              2
-        11              3
-        fruitCount      (fruitCount+5-1)/5
-         */
         session.setAttribute("pageCount",pageCount);
 
-        //此处的视图名称是 index
-        //那么thymeleaf会将这个 逻辑视图名称 对应到 物理视图 名称上去
-        //逻辑视图名称 ：   index
-        //物理视图名称 ：   view-prefix + 逻辑视图名称 + view-suffix
-        //所以真实的视图名称是：      /       index       .html
-        super.processTemplate("index",request,response);
+        return "index" ;
     }
 }
--- src/main/java/myssm/myspringmvc/DispatcherServlet.java
+++ src/main/java/myssm/myspringmvc/DispatcherServlet.java
@@ -19,19 +19,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.HashMap;
 import java.util.Map;
 
 @WebServlet("*.do")
-public class DispatcherServlet extends HttpServlet {
+public class DispatcherServlet extends ViewBaseServlet{
 
     private Map<String,Object> beanMap = new HashMap<>();
 
     public DispatcherServlet(){
     }
 
-    @Override
-    public void init(){
+    public void init() throws ServletException {
+        super.init();
         try {
             InputStream inputStream = getClass().getClassLoader().getResourceAsStream("applicationContext.xml");
             //1.创建DocumentBuilderFactory
@@ -51,14 +52,10 @@ public class DispatcherServlet extends HttpServlet {
                     String className = beanElement.getAttribute("class");
                     Class controllerBeanClass = Class.forName(className);
                     Object beanObj = controllerBeanClass.newInstance() ;
-                    // 创建 setServletContext 是为了在 ViewBaseServlet 中传入 servletContext
-                    Method setServletContextMethod = controllerBeanClass.getDeclaredMethod("setServletContext",ServletContext.class);
-                    setServletContextMethod.invoke(beanObj , this.getServletContext());
-
                     beanMap.put(beanId , beanObj) ;
                 }
             }
-        } catch (ParserConfigurationException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException | InstantiationException | IllegalAccessException | IOException | SAXException e) {
+        } catch (ParserConfigurationException | SAXException | ClassNotFoundException | InstantiationException | IllegalAccessException | IOException e) {
             e.printStackTrace();
         }
     }
@@ -71,34 +68,76 @@ public class DispatcherServlet extends HttpServlet {
         //那么servletPath是：    /hello.do
         // 我的思路是：
         // 第1步： /hello.do ->   hello   或者  /fruit.do  -> fruit
+        // 第2步： hello -> HelloController 或者 fruit -> FruitController
         String servletPath = request.getServletPath();
         servletPath = servletPath.substring(1);
         int lastDotIndex = servletPath.lastIndexOf(".do") ;
         servletPath = servletPath.substring(0,lastDotIndex);
 
-        // 第2步： hello -> HelloController 或者 fruit -> FruitController
-        // servletPath = fruit
         Object controllerBeanObj = beanMap.get(servletPath);
 
-        // 获取请求的方法
         String operate = request.getParameter("operate");
         if(StringUtil.isEmpty(operate)){
             operate = "index" ;
         }
 
         try {
-            // 原来是获取提供的所有方法,与请求的做匹配
-            // 现在直接去找那个operate要的方法
-            //  public Method getDeclaredMethod(String name, Class<?>... parameterTypes)
-            // 直接得到 private void edit(HttpServletRequest request , HttpServletResponse response)之类的......
-            Method method = controllerBeanObj.getClass().getDeclaredMethod(operate,HttpServletRequest.class,HttpServletResponse.class);
-            if(method!=null){
-                method.setAccessible(true);
-                method.invoke(controllerBeanObj,request,response);
+            Method[] methods = controllerBeanObj.getClass().getDeclaredMethods();
+            for(Method method : methods){
+                if(operate.equals(method.getName())){
+                    //1.统一获取请求参数
+
+                    //1-1.获取当前方法的参数，返回参数数组
+                    Parameter[] parameters = method.getParameters();
+                    //1-2.parameterValues 用来承载参数的值
+                    Object[] parameterValues = new Object[parameters.length];
+                    for (int i = 0; i < parameters.length; i++) {
+                        Parameter parameter = parameters[i];
+                        String parameterName = parameter.getName() ;
+                        //如果参数名是request,response,session 那么就不是通过请求中获取参数的方式了
+                        if("request".equals(parameterName)){
+                            parameterValues[i] = request ;
+                        }else if("response".equals(parameterName)){
+                            parameterValues[i] = response ;
+                        }else if("session".equals(parameterName)){
+                            parameterValues[i] = request.getSession() ;
+                        }else{
+                            //从请求中获取参数值
+                            String parameterValue = request.getParameter(parameterName);
+                            String typeName = parameter.getType().getName();
+
+                            Object parameterObj = parameterValue ;
+
+                            if(parameterObj!=null) {
+                                if ("java.lang.Integer".equals(typeName)) {
+                                    parameterObj = Integer.parseInt(parameterValue);
+                                }
+                            }
+
+                            parameterValues[i] = parameterObj ;
+                        }
+                    }
+                    //2.controller组件中的方法调用
+                    method.setAccessible(true);
+                    Object returnObj = method.invoke(controllerBeanObj,parameterValues);
+
+                    //3.视图处理
+                    String methodReturnStr = (String)returnObj ;
+                    if(methodReturnStr.startsWith("redirect:")){        //比如：  redirect:fruit.do
+                        String redirectStr = methodReturnStr.substring("redirect:".length());
+                        response.sendRedirect(redirectStr);
+                    }else{
+                        super.processTemplate(methodReturnStr,request,response);    // 比如：  "edit"
+                    }
+                }
+            }
+
+            /*
             }else{
                 throw new RuntimeException("operate值非法!");
             }
-        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            */
+        } catch (IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
         }
     }
